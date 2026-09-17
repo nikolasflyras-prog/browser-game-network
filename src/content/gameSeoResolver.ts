3372045d7455bebdb165a7036c86a830f7c96006
nikolasflyras-prog <nikolasflyras@gmail.com>
2026-09-17 10:02:53 -0400

    Fix finance guide resolver export

--- src/content/gameSeoResolver.ts
+++ src/content/gameSeoResolver.ts
@@ -3,13 +3,13 @@ import { getExtraGameSeoContent } from "./gameSeoExtras";
 import { getBatch7GameSeoContent } from "./gameSeoBatch7";
 import { getSemiconductorGameSeoContent } from "./gameSeoSemiconductors";
 import { getFinanceGameSeoContent } from "./gameSeoFinance";
-import { getFinanceRebuildGameSeoContent } from "./gameSeoFinanceRebuild";
+import { getFinanceRebuildSeoContent } from "./gameSeoFinanceRebuild";
 
 export function getAnyGameSeoContent(slug: string): GameSeoContent | undefined {
   return getGameSeoContent(slug)
     ?? getExtraGameSeoContent(slug)
     ?? getBatch7GameSeoContent(slug)
     ?? getSemiconductorGameSeoContent(slug)
-    ?? getFinanceRebuildGameSeoContent(slug)
+    ?? getFinanceRebuildSeoContent(slug)
     ?? getFinanceGameSeoContent(slug);
 }
